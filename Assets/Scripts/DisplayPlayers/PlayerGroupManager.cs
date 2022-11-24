097032c81f00311098c803ee44e1d2c9809fd56e
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2022-11-24 10:43:38 +0100

    Display playernames in Lobby
    Display playernames in Lobby

---
+++ Assets/Scripts/DisplayPlayers/PlayerGroupManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class PlayerGroupManager : MonoBehaviour
+{
+    public TextMeshProUGUI namesDisplay;
+    public string[] usernames;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        usernames = new string[PhotonNetwork.PlayerList.Length];
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            usernames[i] = PhotonNetwork.PlayerList[i].NickName;
+            namesDisplay.text = string.Join("\n", usernames);
+        }
+    }
+}
