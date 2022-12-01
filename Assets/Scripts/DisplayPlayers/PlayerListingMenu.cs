ebb734e596e3b8008edd9dcbe7c98eef9b46df6e
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2022-12-01 11:35:54 +0100

    displaying the players in the lobby

---
+++ Assets/Scripts/DisplayPlayers/PlayerListingMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class PlayerListingMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform _content;
+    [SerializeField]
+    private PlayerList _playerList;
+
+    private List<PlayerList> _listings = new List<PlayerList>();
+
+    private void Awake()
+    {
+        GetCurrentPlayers();
+    }
+
+    private void GetCurrentPlayers()
+    {
+        foreach (KeyValuePair<int, Player> playerInfo in PhotonNetwork.CurrentRoom.Players)
+        {
+            AddPLayerListing(playerInfo.Value);
+        }
+    }
+
+    private void AddPLayerListing(Player player)
+    {
+        PlayerList listing = Instantiate(_playerList, _content);
+        if (listing != null)
+        {
+            listing.SetPlayerInfo(player);
+            _listings.Add(listing);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("I entered");
+        AddPLayerListing(newPlayer);
+        
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        int index = _listings.FindIndex(x => x.Player == otherPlayer);
+        if (index != -1)
+        {
+            Destroy(_listings[index].gameObject);
+            _listings.RemoveAt(index);
+        }
+    }
+
+}
