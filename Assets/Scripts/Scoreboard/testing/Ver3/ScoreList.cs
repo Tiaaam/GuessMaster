4662af5b6586c2a9db37a652a4de009da26daae4
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2023-06-14 13:44:44 +0200

    remove code

--- Assets/Scripts/Scoreboard/testing/Ver3/ScoreList.cs
+++ Assets/Scripts/Scoreboard/testing/Ver3/ScoreList.cs
@@ -12,76 +12,17 @@ public class ScoreList : MonoBehaviourPunCallbacks
     private TextMeshProUGUI playerName;
     [SerializeField]
     private TextMeshProUGUI score;
-    public Player player; //{ get; private set; }
-
-    public void SetPlayerInfo(Player player)
-    {
-        /*
-        try
-        {
-            Debug.Log((int)PhotonNetwork.CurrentRoom.CustomProperties["score" + player.ActorNumber]);
-            Debug.Log("B------------------------------------------------------------");
-        }
-        catch
-        {
-            Player = player;
-            playerName.text = player.NickName;
-            Debug.Log("A------------------------------------------------------------");
-            Debug.Log("Antworten Vergleich Listenlaenge:" + player.ActorNumber);
-            //ebug.Log(player.CustomProperties.ContainsKey("Score"));
-            //Debug.Log((int)PhotonNetwork.CurrentRoom.CustomProperties["score" + player.ActorNumber]);
-            Debug.Log(PhotonNetwork.CurrentRoom);
-            Debug.Log("A------------------------------------------------------------");
-            int scoreInt = (int)player.CustomProperties["Score"];
-            score.text = scoreInt.ToString();
-            int rankInt = (int)player.CustomProperties["Rank"];
-            rank.text = rankInt.ToString() + ".";
-
-        }
-        */
-
-
-    }
+    public Player player; 
 
     public void Initialize(Player player)
     {
         playerName.text = player.NickName;
-
         this.player = player;
-
-
         int scoreInt = (int)player.CustomProperties["Score"];
         score.text = scoreInt.ToString();
         int rankInt = (int)player.CustomProperties["Rank"];
         rank.text = rankInt.ToString() + ".";
-
-
-        //UpdateStats();
     }
-
-    /*
-    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
-    {
-        Debug.Log("T------------------------------------------------------------");
-        if (targetPlayer == player)
-        {
-            if (changedProps.ContainsKey("Score"))
-            {
-                UpdateStats();
-            }
-        }
-    }
-
-    void UpdateStats()
-    {
-        Debug.Log("O------------------------------------------------------------");
-        var hash = player.CustomProperties;
-        Debug.Log(hash["Score"].ToString());
-        score.text = hash["Score"].ToString();
-
-        
-    }
-    */
 
     void Update()
     {
--- Assets/Scripts/Scoreboard/testing/Ver3/ScoreListingMenu.cs
+++ Assets/Scripts/Scoreboard/testing/Ver3/ScoreListingMenu.cs
@@ -8,54 +8,6 @@ using System.Linq;
 
 public class ScoreListingMenu : MonoBehaviourPunCallbacks
 {
-    /*
-    [SerializeField]
-    private Transform _content;
-    [SerializeField]
-    private ScoreList _scoreList;
-
-    private List<ScoreList> _listings = new List<ScoreList>();
-    
-    private void Awake()
-    {
-        GetCurrentPlayers();
-    }
-    private void GetCurrentPlayers()
-    {
-        foreach (KeyValuePair<int, Player> playerInfo in PhotonNetwork.CurrentRoom.Players)
-        {
-            AddPLayerListing(playerInfo.Value);
-        }
-    }
-
-    private void AddPLayerListing(Player player)
-    {
-        Debug.Log("AddPlay");
-        ScoreList listing = Instantiate(_scoreList, _content);
-        if (listing != null)
-        {
-            listing.SetPlayerInfo(player);
-            _listings.Add(listing);
-        }
-    }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        Debug.Log("I entereddddd");
-        AddPLayerListing(newPlayer);
-
-    }
-
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        int index = _listings.FindIndex(x => x.Player == otherPlayer);
-        if (index != -1)
-        {
-            Destroy(_listings[index].gameObject);
-            _listings.RemoveAt(index);
-        }
-    }
-    */
     [SerializeField]
     private Transform container;
     [SerializeField]
