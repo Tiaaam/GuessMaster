1f14911a898f13795ccb2a752d9aea5cb4ca263b
TimSchrody <115226610+TimSchrody@users.noreply.github.com>
2023-06-15 13:09:40 +0200

    RoomScene script updated

--- Assets/Scripts/DisplayPlayers/PlayerList.cs
+++ Assets/Scripts/DisplayPlayers/PlayerList.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 using TMPro;
 using Photon.Realtime;
+using Photon.Pun;
 
-public class PlayerList : MonoBehaviour
+public class PlayerList : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     private TextMeshProUGUI nametext;
-    public Player Player { get; private set; }
+    [SerializeField]
+    private GameObject kickButton;
+    public Player Player;// { get; private set; }
 
     public void SetPlayerInfo(Player player)
     {
         Player = player;
         nametext.text = player.NickName;
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            kickButton.SetActive(false);
+        }
+        if (player.IsMasterClient)
+        {
+            //kickButton.SetActive(false);
+        }
     }
+    [PunRPC]
+    private void Kick_Player()
+    {
+        PhotonNetwork.LeaveRoom(); // load lobby scene, returns to master server
+    }
+
+    public void OnClickKickPlayer()
+    {
+        PhotonNetwork.EnableCloseConnection = true;
+        //PhotonNetwork.CloseConnection(Player);
+        
+        Debug.Log(Player.NickName);
+        Debug.Log("Kicked Player from this Room");
+
+        photonView.RPC("KickPlayer", Player, "kick");
+    }
+
+
+
+
 }
